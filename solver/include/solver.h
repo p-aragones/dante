44f3610ecbab9f027eeb82562f9c600a80a59817
Pol <Aragons>
2021-04-06 09:44:26 +0200

    base template for dante's solver

---
+++ solver/include/solver.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPE-200-BAR-2-1-dante-pol.aragones
+** File description:
+** solver
+*/
+
+#ifndef SOLVER_H_
+#define SOLVER_H_
+
+#include "utils.h"
+
+#endif /* !SOLVER_H_ */
---
+++ solver/src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** minishell1
+** File description:
+** remake of tcsh
+*/
+
+#include "solver.h"
+
+int main(void)
+{
+    my_putstr("hello world\n");
+    return (0);
+}
