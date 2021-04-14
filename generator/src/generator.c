9d1f20f6db94c73fd5d3d9b3128b4c5d1bdc0d46
Adria <adria.marata-fernandez@epitech.eu>
2021-04-14 10:54:14 +0200

    push of random maze created

--- generator/src/generator.c
+++ generator/src/generator.c
@@ -7,8 +7,30 @@
 
 #include "generator.h"
 
+char create_cell(char **maze, int y, int i, int c)
+{
+    int b = rand() % 3;
+
+    if (b == 2)
+        maze[i][c] = 'X';
+    else
+        maze[i][c] = '*';
+    return (maze[i][c]);
+}
+
 char **create_maze(char **maze, int x, int y)
 {
+    int i = 0;
+    int c = 0;
+
+    while (i < y) {
+        c = 0;
+        while (c < x) {
+            maze[i][c] = create_cell(maze, y, i, c);
+            c++;
+        }
+        i++;
+    }
     return (maze);
 }
 
@@ -22,7 +44,7 @@ int generator(int x, int y)
         maze[i] = malloc(sizeof(char) * x);
         c = 0;
         while (c < x) {
-            maze[i][c] = 'B';
+            maze[i][c] = 'e';
             c++;
         }
         i++;
